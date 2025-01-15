e845898728cd81017c218b364acc9aa34ac6ddb7
Sonu Kumar <128314830+sonukds@users.noreply.github.com>
2025-01-15 13:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+function daysOfAYear(year) {
+    const isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    return isLeapYear ? 366 : 365;
+}
+
+// Examples
+console.log(daysOfAYear(2022)); 
+console.log(daysOfAYear(2024)); 
+console.log(daysOfAYear(1900)); 
+console.log(daysOfAYear(2000)); 
